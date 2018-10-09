973caeddedf92193307ea37bf957dc2cebea4aa9
7Dan2 <unic.norton@gmail.com>
2018-10-09 16:33:16 +0200

    Switching content to Angular and adding favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
