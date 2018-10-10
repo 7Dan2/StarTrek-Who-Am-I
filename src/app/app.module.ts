09da40242bca3f3fa87c68e27e284f0748d46ab7
hdk <amir.madi34@gmail.com>
2018-10-10 16:01:51 +0200

    test fusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { JumbotronComponent } from './elements/jumbotron/jumbotron.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './../../../project/src/app/pages/home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CardsComponent } from './cards/cards.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
