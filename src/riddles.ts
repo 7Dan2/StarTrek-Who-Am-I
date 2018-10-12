091566f724b4439af8d5c0e65939208a5c6ea0be
7Dan2 <unic.norton@gmail.com>
2018-10-12 09:16:33 +0200

    update help button

---
+++ src/riddles.ts
@@ -0,0 +1,27 @@
+export class RiddlesPlanet {
+    RiddleMars: {
+        "string", ""}
+    RiddleJupiter: {
+        "Je suis deux fois plus massive que toutes les planetes réunies", "Je suis une géante gazeuse", "J'ai un gros oeil rouge à ma surface"}
+    RiddleMoon: {
+        "string", ""}
+    RiddleVenus: {
+        "string", ""}
+    RiddlePlanetX: {
+        "string", ""}
+    RiddleUranus: {
+        "string", ""}
+    RiddlePluton: {
+        "string", ""}
+    RiddleSaturn: {
+        "string", ""}
+}
+
+
+
+
+ 
+  
+  
+  
+
