4eb7b90191f4bd63a4bcb34e51a5eb20831c342d
hdk <amir.madi34@gmail.com>
2018-10-14 13:02:28 +0200

    Succesfully created the right object stucture

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet } from '../planet';
 
 @Component({
   selector: 'app-cards',
@@ -7,7 +8,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  planet:Planet = {
+
+     
+    info:
+    { 
+      "Mercury":"https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png" ,
+      "Venus":"https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png",
+      "Earth":"https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png",
+      "Mars":"https://solarsystem.nasa.gov/system/stellar_items/list_view_images/6_mars_480x320.jpg",
+      "Jupiter":"https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png",
+      "Saturn":"https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png",
+      "Uranus":"https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png",
+      "Neptune":"https://solarsystem.nasa.gov/system/feature_items/images/30_neptune.png"
+    },
+
+    sat:{
+
+      "Mercury": 0,
+      "Venus": 0,
+      "Earth": 1,
+      "Mars": 2,
+      "Jupiter": 79,
+      "Saturn": 61,
+      "Uranus": 27,
+      "Neptune": 14
+   },
+   data: {
+
+    name: ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"],
+    image: [
+            "https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png",
+            "https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png",
+            "https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png",
+            "https://solarsystem.nasa.gov/system/stellar_items/list_view_images/6_mars_480x320.jpg",
+            "https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png",
+            "https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png",
+            "https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png",
+            "https://solarsystem.nasa.gov/system/feature_items/images/30_neptune.png"
+    ],
+    enigme: {
+
+            "Jupiter": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Mercury":  [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Venus": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Saturn": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Earth": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Mars": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Uranus": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            "Neptune": [
+              ["Je suis deux fois plus massive que toutes les planetes"],
+              ["Je suis une géante gazeuse"],
+              ["J'ai un gros point rouge à ma surface"]              
+             ],
+            
+    }
+
+    },
+
+  }
+  
+
+  planetName:string;
+  planetImage:string;
+  planetEnigme:string;
+  
+    
+  constructor() {
+    this.planetName = this.planet.data.name;
+    this.planetImage = this.planet.data.image;
+    this.planetEnigme = this.planet.data.enigme;
+
+    console.log(this.planetName);
+    console.log(this.planetImage);
+    console.log(this.planetEnigme["Jupiter"]);
+   }
+
 
   ngOnInit() {
   }
