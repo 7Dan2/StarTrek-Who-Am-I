f0700faddcea5e437562250f82c26aac21d85596
hdk <amir.madi34@gmail.com>
2018-10-18 06:45:40 +0200

    ÃFirst version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public name:string ="apod.jpg";
+  setMyStyles() {
+    let styles = {
+      'background': 'url( ../../../assets/' + this.name + ') no-repeat center center fixed',
+      'background-size': 'cover',
+      'height': '100%',
+      'overflow': 'hidden',
+      'font-family': 'Arial, Helvetica, sans-serif',
+    };
+    return styles;
+  }
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.name="apod2.jpg";
+    }, 10000);
+    setTimeout(() => {
+      this.name="apod3.jpg";
+    }, 20000);
+    setTimeout(() => {
+      this.name="apod3.jpg";
+    }, 30000);
+    
   }
 
 }
