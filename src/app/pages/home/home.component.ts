0b341e181413ea6ca9aae0acb940e7bafec22384
hdk <amir.madi34@gmail.com>
2018-10-18 07:14:03 +0200

    Working fine

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public name:string ="apod.jpg";
+  public name:string ="../../../assets/apod.jpg";
   setMyStyles() {
     let styles = {
-      'background': 'url( ../../../assets/' + this.name + ') no-repeat center center fixed',
+      'background': 'url(' + this.name + ') no-repeat center center fixed',
       'background-size': 'cover',
       'height': '100%',
       'overflow': 'hidden',
@@ -20,16 +20,24 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+   
+
     setTimeout(() => {
-      this.name="apod2.jpg";
+      this.name="https://apod.nasa.gov/apod/image/1801/Arp243_Hubble_3978.jpg";
+      this.setMyStyles();
     }, 10000);
     setTimeout(() => {
-      this.name="apod3.jpg";
+      this.name="https://apod.nasa.gov/apod/image/1810/PIA22549_fig1.jpg";
+      this.setMyStyles();
     }, 20000);
-    setTimeout(() => {
-      this.name="apod3.jpg";
-    }, 30000);
     
+
+    setTimeout(() => {
+      this.name="../../../assets/apod3.jpg";
+      this.setMyStyles();
+    }, 30000);
+
+   
   }
 
 }
